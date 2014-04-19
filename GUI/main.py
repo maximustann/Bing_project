f2d6050106badd575d6e2d4dc3710f483b2d92b5
Max <titantbx1989215@gmail.com>
2014-04-19 15:31:44 +1200

    nothing

--- GUI/main.py
+++ GUI/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python
+
+from PyQt4 import QtGui, QtCore
+
+class Bing_MainWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(Bing_MainWindow, self).__init__()
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication([])
+    bing_p = Bing_MainWindow()
+    bing_p.show()
+    app.exec_()
