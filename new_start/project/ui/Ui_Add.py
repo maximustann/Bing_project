5339e46b5b930918ea598f149a364f7f619e1cd1
Shenbo Xuan <xuanshen@gare-fare.ecs.vuw.ac.nz>
2014-07-07 19:34:20 +1200

    update ui

--- new_start/project/ui/Ui_Add.py
+++ new_start/project/ui/Ui_Add.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Add.ui'
 #
-# Created: Mon Jul  7 19:16:30 2014
+# Created: Mon Jul  7 19:33:18 2014
 #      by: PyQt4 UI code generator 4.10.2
 #
 # WARNING! All changes made in this file will be lost!
