00cbd23cfa3b24e538aec5e51601f4ef8ac3a9b0
Max <titantbx1989215@gmail.com>
2014-07-29 00:11:29 +1200

    check if file exist

--- new_start/UI_File/new_do_everything.sh
+++ new_start/UI_File/new_do_everything.sh
@@ -6,6 +6,11 @@ do
 	name=`expr substr "$i" 1 $p`
 	pyuic4 -x $i -o ${name}.py
 	sed -i "5d" ${name}.py
+	if [ ! -e ../project/ui/"Ui_"${name}.py ]
+	then
+		cp ${name}.py ../project/ui/"Ui_"${name}.py
+	fi
+
 	diff -u ${name}.py ../project/ui/"Ui_"${name}.py
 	if [ 0 -ne $? ]
 	then
