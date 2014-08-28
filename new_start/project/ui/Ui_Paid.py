42f0102a1918fa9544dd725e8486c26f0e9fd913
Max <titantbx1989215@gmail.com>
2014-08-28 15:51:05 +1200

    change Paid Ui

--- new_start/project/ui/Ui_Paid.py
+++ new_start/project/ui/Ui_Paid.py
@@ -26,30 +26,140 @@ class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(312, 172)
+        Dialog.setStyleSheet(_fromUtf8("#Dialog {\n"
+"   background-color: beige;\n"
+"}\n"
+"\n"
+"/* Nice Windows-XP-style password character. */\n"
+"QLineEdit[echoMode=\"2\"] {\n"
+"    lineedit-password-character: 9679;\n"
+"}\n"
+"\n"
+"/* We provide a min-width and min-height for push buttons\n"
+"   so that they look elegant regardless of the width of the text. */\n"
+"QPushButton {\n"
+"    background-color: palegoldenrod;\n"
+"    border-width: 2px;\n"
+"    border-color: darkkhaki;\n"
+"    border-style: solid;\n"
+"    border-radius: 5;\n"
+"    padding: 3px;\n"
+"    min-width: 9ex;\n"
+"    min-height: 2.5ex;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"   background-color: khaki;\n"
+"}\n"
+"\n"
+"/* Increase the padding, so the text is shifted when the button is\n"
+"   pressed. */\n"
+"QPushButton:pressed {\n"
+"    padding-left: 5px;\n"
+"    padding-top: 5px;\n"
+"    background-color: #d0d67c;\n"
+"}\n"
+"\n"
+"QLabel, QAbstractButton {\n"
+"    font: bold;\n"
+"}\n"
+"\n"
+"/* Mark mandatory fields with a brownish color. */\n"
+".mandatory {\n"
+"    color: brown;\n"
+"}\n"
+"\n"
+"/* Bold text on status bar looks awful. */\n"
+"QStatusBar QLabel {\n"
+"   font: normal;\n"
+"}\n"
+"\n"
+"QStatusBar::item {\n"
+"    border-width: 1;\n"
+"    border-color: darkkhaki;\n"
+"    border-style: solid;\n"
+"    border-radius: 2;\n"
+"}\n"
+"\n"
+"QComboBox, QLineEdit, QSpinBox, QTextEdit, QListView {\n"
+"    background-color: cornsilk;\n"
+"    selection-color: #0a214c; \n"
+"    selection-background-color: #C19A6B;\n"
+"}\n"
+"\n"
+"QListView {\n"
+"    show-decoration-selected: 1;\n"
+"}\n"
+"\n"
+"QListView::item:hover {\n"
+"    background-color: wheat;\n"
+"}\n"
+"\n"
+"/* We reserve 1 pixel space in padding. When we get the focus,\n"
+"   we kill the padding and enlarge the border. This makes the items\n"
+"   glow. */\n"
+"QLineEdit, QFrame {\n"
+"    border-width: 2px;\n"
+"    padding: 1px;\n"
+"    border-style: solid;\n"
+"    border-color: darkkhaki;\n"
+"    border-radius: 5px;\n"
+"}\n"
+"\n"
+"/* As mentioned above, eliminate the padding and increase the border. */\n"
+"QLineEdit:focus, QFrame:focus {\n"
+"    border-width: 3px;\n"
+"    padding: 0px;\n"
+"}\n"
+"\n"
+"/* A QLabel is a QFrame ... */\n"
+"QLabel {\n"
+"    border: none;\n"
+"    padding: 0;\n"
+"    background: none;\n"
+"}\n"
+"\n"
+"/* A QToolTip is a QLabel ... */\n"
+"QToolTip {\n"
+"    border: 2px solid darkkhaki;\n"
+"    padding: 5px;\n"
+"    border-radius: 3px;\n"
+"    opacity: 200;\n"
+"}\n"
+"\n"
+"/* Nice to have the background color change when hovered. */\n"
+"QRadioButton:hover, QCheckBox:hover {\n"
+"    background-color: wheat;\n"
+"}\n"
+"\n"
+"/* Force the dialog\'s buttons to follow the Windows guidelines. */\n"
+"QDialogButtonBox {\n"
+"    button-layout: 0;\n"
+"}"))
         self.buttonBox = QtGui.QDialogButtonBox(Dialog)
         self.buttonBox.setGeometry(QtCore.QRect(-130, 110, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
-        self.widget = QtGui.QWidget(Dialog)
-        self.widget.setGeometry(QtCore.QRect(50, 40, 211, 62))
-        self.widget.setObjectName(_fromUtf8("widget"))
-        self.gridLayout = QtGui.QGridLayout(self.widget)
+        self.layoutWidget = QtGui.QWidget(Dialog)
+        self.layoutWidget.setGeometry(QtCore.QRect(50, 40, 211, 62))
+        self.layoutWidget.setObjectName(_fromUtf8("layoutWidget"))
+        self.gridLayout = QtGui.QGridLayout(self.layoutWidget)
         self.gridLayout.setMargin(0)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.label = QtGui.QLabel(self.widget)
+        self.label = QtGui.QLabel(self.layoutWidget)
         self.label.setObjectName(_fromUtf8("label"))
         self.horizontalLayout.addWidget(self.label)
-        self.lineEdit = QtGui.QLineEdit(self.widget)
+        self.lineEdit = QtGui.QLineEdit(self.layoutWidget)
         self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
         self.horizontalLayout.addWidget(self.lineEdit)
         self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 2)
-        self.label_2 = QtGui.QLabel(self.widget)
+        self.label_2 = QtGui.QLabel(self.layoutWidget)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
-        self.lineEdit_2 = QtGui.QLineEdit(self.widget)
+        self.lineEdit_2 = QtGui.QLineEdit(self.layoutWidget)
         self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
         self.gridLayout.addWidget(self.lineEdit_2, 1, 1, 1, 1)
 
